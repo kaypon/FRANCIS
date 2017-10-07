2f6d588da8d4685fb8fbddb8f82499c3bffdd1c9
Kevin van der Poll <vdpkevin@gmail.com>
2017-10-07 15:38:35 -0400

    created welcome page, got styling to work, cleared up some links

--- Gemfile
+++ Gemfile
@@ -60,3 +60,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Added at 2017-10-07 15:32:47 -0400 by kevinvanderpoll:
+gem "hirb", "~> 0.7.3"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   resources :photos
   resources :reservations
   resources :hotels
   resources :users
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
